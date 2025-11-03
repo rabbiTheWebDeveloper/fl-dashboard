c1009081970db112540468ab84f4b5e653628708
S M AL RABBI <rabbithedeveloper@gmail.com>
2025-11-03 15:10:31 +0600

    Update page.js

--- src/app/(dashboard)/dashboard/myshop/manage/social-links/page.js
+++ src/app/(dashboard)/dashboard/myshop/manage/social-links/page.js
@@ -5,7 +5,7 @@ import { userInfo } from "@/lib";
 const page = async () => {
   const user = await userInfo();
   const socialLinkInfo = await getSocialLinkQuery(user);
-  console.log("socialLinkInfo:", socialLinkInfo);
+//   console.log("socialLinkInfo:", socialLinkInfo);
   return (
     <>
       <SocialLink user={user} socialLinkInfo={socialLinkInfo?.data} />
