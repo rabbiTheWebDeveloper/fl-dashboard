3c010ca8433459f8a1244cb6c7f79c4dd04222ba
S M AL RABBI <rabbithedeveloper@gmail.com>
2025-11-22 17:17:03 +0600

    Update Subscription.jsx

--- src/app/(dashboard)/_component/Subscription.jsx
+++ src/app/(dashboard)/_component/Subscription.jsx
@@ -306,17 +306,6 @@ const Subscription = () => {
         </div>
       </div>
 
-      {/* Footer */}
-      <div className="bg-gray-900 text-white py-12">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="text-center">
-            <div className="text-2xl font-bold mb-4">ZatiqEasy</div>
-            <p className="text-gray-400">
-              © 2024 ZatiqEasy. All rights reserved.
-            </p>
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
